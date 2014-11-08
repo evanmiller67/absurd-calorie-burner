58e2ffdc6c5f244832b352e1fc4d0e1c0d3c779b
Dana Jones <djones@changehealthcare.com>
2014-11-08 12:18:26 -0600

    Add foods table, and really truly push the item migrations this time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141108175749) do
+ActiveRecord::Schema.define(:version => 20141108181129) do
 
   create_table "exercises", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(:version => 20141108175749) do
     t.integer  "met"
     t.string   "title"
     t.integer  "item_id"
+  end
+
+  create_table "foods", :force => true do |t|
+    t.string   "name"
+    t.integer  "calories"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "items", :force => true do |t|
