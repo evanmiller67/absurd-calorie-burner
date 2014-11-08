cdf8e268fc1c5940c9cf6c7471739bc152653cba
Evan Miller <evanmiller67@gmail.com>
2014-11-08 10:38:22 -0600

    add additional weight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,10 +14,11 @@ module ApplicationHelper
     Calculate.kg_to_lb(additional_weight)
   end
 
-  def find_time_from_calories_gender_height_weight_age(calories, gender, height, weight, age)
-    weight_in_kg = Calculate.lb_to_kg(weight)
-    height_in_cm = Calculate.in_to_cm(height)
-    bmr          = Calculate.bmr_from_gender_height_weight_age(gender, height_in_cm, weight_in_kg, age)
+  def find_time_from_calories_gender_height_weight_additional_weight_age(calories, gender, height, weight, additional_weight, age)
+    weight_in_kg            = Calculate.lb_to_kg(weight)
+    additional_weight_in_kg = Calculate.lb_to_kg(additional_weight)
+    height_in_cm            = Calculate.in_to_cm(height)
+    bmr                     = Calculate.bmr_from_gender_height_weight_age(gender, height_in_cm, weight_in_kg + additional_weight_in_kg, age)
 
     Calculate.time_from_calories_mets_bmr(calories, mets, bmr) * 60.0
   end
