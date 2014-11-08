b475a56f183c9e176a22c926ee3c537548d6289e
Dana Jones <djones@changehealthcare.com>
2014-11-07 22:33:43 -0600

    Calculate basic HB RMR given demographic inputs

--- Gemfile
+++ Gemfile
@@ -23,6 +23,10 @@ end
 gem 'jquery-rails'
 gem 'rspec-rails'
 
+group :development do
+  gem 'pry'
+end
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
+require "lib/exercise_calculator"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
@@ -22,7 +23,7 @@ module AbsurdCalorieBurner
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    # config.autoload_paths += %W(#{config.root}/lib/)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
